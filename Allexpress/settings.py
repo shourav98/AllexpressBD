d0c6352c28d9e20d8008607e7d39cd9fba8ca6f5
shourav98 <mdmominulshourav@gmail.com>
2025-02-27 19:30:03 +0600

    Allowed host

--- Allexpress/settings.py
+++ Allexpress/settings.py
@@ -17,7 +17,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = ["allexpressbd-1.onrender.com", "localhost", "127.0.0.1"]
+
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[
     'https://sandbox.sslcommerz.com',  # Add SSLCommerz domain here
