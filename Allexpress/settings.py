5127cca8297184bcca2e39df1cb17751976f0d63
Md Mominul Islam <mdmominulshourav@gmail.com>
2025-09-22 13:35:18 +0600

    Allowed host onrender added

--- Allexpress/settings.py
+++ Allexpress/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
